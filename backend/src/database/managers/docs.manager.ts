fed9950ffa1a67a0cabe733d4f81a5d6dd4b47c8
Nimplex <39964594+Nimplex@users.noreply.github.com>
2022-01-18 23:06:28 +0100

    upload docs

--- backend/src/database/managers/docs.manager.ts
+++ backend/src/database/managers/docs.manager.ts
@@ -1,9 +1,58 @@
+import { Schema, Document, model } from "mongoose"
 import { Core } from "../../"
+
+const schema = new Schema(
+	{
+		author: String, // id ex. 364056796932997121
+		nick: String,
+		age: Number,
+		voice: Number,
+		long: String,
+		short: String,
+		steam: String,
+	},
+	{ timestamps: true }
+)
+
+export interface Podanie extends Document {
+	author: string // id ex. 364056796932997121
+	nick: string
+	age: number
+	voice: number
+	long: string
+	short: string
+	steam: string
+	docID?: number
+}
+
+const DocsModel = model<Podanie>("podania", schema)
 
 export class DocsManager {
 	private readonly core: Core
 
 	constructor(core: Core) {
 		this.core = core
+	}
+
+	async get(docID: number): Promise<Podanie | null> {
+		return await DocsModel.findOne({ docID })
+	}
+
+	async create(doc: {
+		author: string
+		nick: string
+		age: number
+		voice: number
+		long: string
+		short: string
+		steam: string
+	}): Promise<Podanie | null> {
+		const docID =
+			((await DocsModel.findOne().sort({ _id: -1 }))?.docID || 0) + 1
+
+		return await DocsModel.create({
+			docID,
+			...doc,
+		})
 	}
 }
--- backend/src/http/router/routes/docs.route.ts
+++ backend/src/http/router/routes/docs.route.ts
@@ -67,16 +67,33 @@ router.get("/publish", adminCheck, (req, res) => {})
 /*
 	* /api/docs/upload
 	! Złóż podanie do sprawdzenia
-
-	?{
-	?	userTag: string
-	?	userID: string
-	?	userSteam: string
-	?	userAge: int
-	?	shortDescription: string
-	?	longDescription: string
-	?}
 */
-router.put("/upload", (req, res) => {})
+router.put("/upload", async (req, res) => {
+	const { author, nick, age, voice, long, short, steam } = req.body
+
+	if (!nick || !author || !steam || !age || !short || !long)
+		return res.json({ code: 1, message: 1 })
+	if (long.length < 200) return res.json({ code: 1, message: 2 })
+	if (short.length < 30) return res.json({ code: 1, message: 3 })
+	if (age < 12 || age > 99) return res.json({ code: 1, message: 4 })
+	if (nick.length == 0 || nick !== (<any>req.session).userid)
+		return res.json({ code: 1, message: 5 })
+	if (author.length == 0 || author !== req.session.id)
+		return res.json({ code: 1, message: 6 })
+	if (steam.length < "https://steamcommunity.com/".length)
+		return res.json({ code: 1, message: 7 })
+
+	const doc = await req.core.database.docs.create({
+		author,
+		nick,
+		age,
+		voice,
+		long,
+		short,
+		steam,
+	})
+
+	res.json({ code: 200, message: "Fine!", docID: doc?.docID })
+})
 
 export default router
