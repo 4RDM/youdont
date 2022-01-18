20297684c81a9e880cd485206cc8425f077708a1
Nimplex <39964594+Nimplex@users.noreply.github.com>
2022-01-18 22:35:50 +0100

    Permissions

--- backend/src/database/database.ts
+++ backend/src/database/database.ts
@@ -6,12 +6,14 @@ import { ShortsManager } from "./managers/shorts.manager"
 import logger from "../utils/logger"
 import config from "../config"
 import { Core } from "../"
+import { SettingManager } from "./managers/settings.manager"
 
 export default class Database {
 	public readonly donates: DonatesManager
 	public readonly users: UsersManager
 	public readonly docs: DocsManager
 	public readonly shorts: ShortsManager
+	public readonly settings: SettingManager
 
 	constructor(core: Core) {
 		// prettier-ignore
@@ -19,6 +21,7 @@ export default class Database {
 		this.users = new UsersManager(core)
 		this.docs = new DocsManager(core)
 		this.shorts = new ShortsManager(core)
+		this.settings = new SettingManager(core)
 
 		mongoose
 			.connect(
---
+++ backend/src/database/managers/settings.manager.ts
@@ -0,0 +1,53 @@
+import { readFileSync } from "fs"
+import { join } from "path"
+import { Core } from "../../"
+
+export type Permission =
+	| "MANAGE_USERS"
+	| "MANAGE_SHORTS"
+	| "MANAGE_DOCS"
+	| "ADMINISTRATOR"
+
+export interface User {
+	discordID: string
+	nickname: string
+	avatar: string
+	userID: string
+	permissions: Permission[]
+}
+
+export interface Settings {
+	users: User[]
+	docsOpen: boolean
+}
+
+export class SettingManager {
+	private readonly core: Core
+	private readonly path = join(__dirname, "..", "..", "..", "data.json")
+	private settings: Settings
+
+	constructor(core: Core) {
+		this.core = core
+		this.settings = JSON.parse(
+			readFileSync(this.path, { encoding: "utf-8" })
+		)
+	}
+
+	getUser(discordID: string): User | undefined {
+		return this.settings.users.find(user => user.discordID === discordID)
+	}
+
+	hasPermission(discordID: string, permission: Permission): boolean {
+		const user = this.settings.users.find(
+			user => user.discordID === discordID
+		)
+
+		if (!user) return false
+		else if (
+			user.permissions.includes("ADMINISTRATOR") ||
+			user.permissions.includes(permission)
+		)
+			return true
+		else return false
+	}
+}
