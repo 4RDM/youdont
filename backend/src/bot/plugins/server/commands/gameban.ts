495c12cac0cb6025215d895274332d5668060e6b
Nimplex <39964594+Nimplex@users.noreply.github.com>
2022-01-22 23:59:14 +0100

    TODO: gameban

---
+++ backend/src/bot/plugins/server/commands/gameban.ts
@@ -0,0 +1,48 @@
+import { Embed } from "../../../../utils/discordEmbed"
+import { Command } from "../../../../types"
+
+const command: Command = {
+	triggers: ["gameban"],
+	description: "Zbanuj osobę na serwerze",
+	permissions: ["ADMINISTRATOR"],
+	async exec(client, message, args) {
+		if (!args[0]) return
+
+		const msg = await message.channel.send({
+			embeds: [
+				Embed({
+					description: "**Wysyłanie**",
+					user: message.author,
+				}),
+			],
+		})
+
+		client.Core.rcon.send(
+			args.join(" "),
+			() => {
+				msg.edit({
+					embeds: [
+						Embed({
+							color: "#1F8B4C",
+							description: "**Wysłano!**",
+							user: message.author,
+						}),
+					],
+				})
+			},
+			() => {
+				msg.edit({
+					embeds: [
+						Embed({
+							color: "#E74C3C",
+							description: "**Wystąpił błąd!**",
+							user: message.author,
+						}),
+					],
+				})
+			}
+		)
+	},
+}
+
+module.exports = command
