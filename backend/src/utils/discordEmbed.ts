2336b671a4d5a01fbb30fae440504ec54834870a
Nimplex <39964594+Nimplex@users.noreply.github.com>
2022-02-02 12:30:31 +0100

    support for thumbnails and images

--- backend/src/utils/discordEmbed.ts
+++ backend/src/utils/discordEmbed.ts
@@ -6,6 +6,8 @@ export interface EmbedStructure {
 	color?: HexColorString
 	fields?: EmbedFieldData[]
 	footer?: boolean
+	thumbnail?: string
+	image?: string
 	user: User
 }
 
@@ -15,6 +17,8 @@ export const Embed = ({
 	color,
 	fields,
 	footer = true,
+	thumbnail,
+	image,
 	user,
 }: EmbedStructure): MessageEmbed => {
 	const embed = new MessageEmbed()
@@ -28,6 +32,8 @@ export const Embed = ({
 		embed
 			.setFooter({ text: `${user.tag} (${user.id})` })
 			.setTimestamp(new Date())
+	if (image) embed.setImage(image)
+	if (thumbnail) embed.setThumbnail(thumbnail)
 
 	return embed
 }
