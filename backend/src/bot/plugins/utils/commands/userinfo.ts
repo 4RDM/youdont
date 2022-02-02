db75f09fe87a1df512412d67c272a24b142c5bc4
Nimplex <39964594+Nimplex@users.noreply.github.com>
2022-02-02 12:53:52 +0100

    userinfo

---
+++ backend/src/bot/plugins/utils/commands/userinfo.ts
@@ -0,0 +1,71 @@
+import { Embed } from "../../../../utils/discordEmbed"
+import { Command } from "../../../../types"
+
+const command: Command = {
+	triggers: ["userinfo", "user"],
+	description: "Sprawdź informacje na temat użytkownika",
+	async exec(client, message, args) {
+		const user = message.mentions.members?.first() || message.member
+		if (!user) return
+
+		await user.user.fetch(true)
+
+		const embed = Embed({
+			title: user.nickname || user.user.tag,
+			fields: [
+				{
+					name: "Nazwa",
+					value: `\`${user.user.tag}\``,
+					inline: true,
+				},
+				{
+					name: "Pseudonim",
+					value: `\`${user.nickname ? user.nickname : "Brak"}\``,
+					inline: true,
+				},
+				{
+					name: "ID",
+					value: `\`${user.id}\``,
+					inline: true,
+				},
+				{
+					name: "Dołączył",
+					value: `<t:${Math.floor(
+						(user.joinedAt?.getTime() || 0) / 1000
+					)}:R>`,
+					inline: true,
+				},
+				{
+					name: "Utworzył konto",
+					value: `<t:${Math.floor(
+						(user.user.createdAt?.getTime() || 0) / 1000
+					)}:R>`,
+					inline: true,
+				},
+				{
+					name: "Booster od",
+					value: `${
+						user.premiumSince
+							? `<t:${Math.floor(
+									(user.premiumSince.getTime() || 0) / 1000
+							  )}:R>`
+							: "`Brak`"
+					}`,
+					inline: true,
+				},
+			],
+			image:
+				user.user.bannerURL({
+					dynamic: true,
+					size: 1024,
+					format: "png",
+				}) || "",
+			thumbnail: user.displayAvatarURL({ dynamic: true }),
+			user: message.author,
+		})
+
+		message.channel.send({ embeds: [embed] })
+	},
+}
+
+module.exports = command
