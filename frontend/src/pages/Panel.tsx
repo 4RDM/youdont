f8679c8e2a49bd1c75f4b9978cf1830e0e5f73ee
Nimplex <39964594+Nimplex@users.noreply.github.com>
2022-02-03 17:23:10 +0100

    TEST

--- frontend/src/pages/Panel.tsx
+++ frontend/src/pages/Panel.tsx
@@ -5,11 +5,33 @@ import { UserContext } from '../provider/UserContext'
 const Panel: FC = () => {
 	const context = useContext(UserContext)
 
+	/*
+	{
+		"user": {
+			"code": 200,
+			"message": "OK",
+			"user": {
+				"userid": "364056796932997121",
+				"tag": "1010",
+				"username": "Nimplex",
+				"email": "a@gmail.com",
+				"avatar": "71b46749cfd16d05f3630139d9383eb5"
+			}
+		}
+	}
+	*/
+
+	const user = Object.values(context.user !== null ? context.user.user : {})
+
 	return (
 		<Container>
 			{(context.user !== null && (
 				<div id="panel-container">
-					<h1>{context.user.user.username}</h1>
+					<div>
+						{user.map((val: any) => {
+							return <h1>{val}</h1>
+						})}
+					</div>
 				</div>
 			)) || (
 				<div className="error-401-container">
