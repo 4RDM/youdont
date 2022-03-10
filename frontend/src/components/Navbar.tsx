b53ab91350586222c6c2ab897fa7ca428e05b76b
Nimplex <39964594+Nimplex@users.noreply.github.com>
2022-03-10 22:07:18 +0100

    final version of mobile navbar

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -8,40 +8,43 @@ const Navbar: FC = (props) => {
 	const [isMobile, setMobile] = useState(false)
 	const [isOpen, setOpen] = useState(false)
 
+	const PanelOrLogout = context?.user == null ? <a href="/api/dashboard/login">Logowanie</a> : location.pathname == "/panel" ? <a href="/api/dashboard/logout">Wyloguj Się</a> : <Link to="/panel">Panel użytkownika</Link>
+
 	const checkIsMobile = () => setMobile((window.matchMedia && window.matchMedia("(max-width: 680px)").matches))
 
 	useEffect(() => {
 		checkIsMobile()
 		window.addEventListener("resize", checkIsMobile)
-		return window.removeEventListener("resize", checkIsMobile)
 	}, [])
+
+	useEffect(() => {
+		if (!isMobile) setOpen(isMobile)
+	}, [isMobile])
 
 	return (
 		<>
+			{/* MOBILE NAVBAR */}
 			<div id="nav-sidebar" style={{display: isOpen ? "flex" : "none"}}>
-				<button id="nav-close-sidebar" onClick={() => setOpen(false)}>×</button>		
+				<button className="nav-sidebar-button" onClick={() => setOpen(false)}>×</button>
 				<div>
-				<Link to="/">Strona głowna</Link>
-				<Link to="/administration">Administracja</Link>
-				<Link to="/articles">Artykuły</Link>
+					<Link to="/">Strona główna</Link>
+					<Link to="/administration">Administracja</Link>
+					<Link to="/articles">Artykuły</Link>
+					{PanelOrLogout}
 				</div>
 			</div>
+			{/* DESKTOP NAVBAR */}
 			<div id="navbar">
 				<h1>4RDM</h1>
-				{!isMobile ? <div id="nav-buttons">
-							<Link to="/">Strona głowna</Link>
-							<Link to="/administration">Administracja</Link>
-							<Link to="/articles">Artykuły</Link>
-							{context?.user == null ?
-								<a href="/api/dashboard/login">Logowanie</a> :
-								location.pathname == "/panel" ? <a href="/api/dashboard/logout">Wyloguj Się</a> :
-									<Link to="/panel">Panel użytkownika</Link>
-							}
-						</div>
-				:	<div id="nav-open-close">
-						<button onClick={() => setOpen(true)}>≡</button>
-					</div>
-				}
+				<div id="nav-buttons">
+					{!isMobile ? (<>
+						<Link to="/">Strona główna</Link>
+						<Link to="/administration">Administracja</Link>
+						<Link to="/articles">Artykuły</Link>
+						{PanelOrLogout}
+					</>): <button className="nav-sidebar-button" onClick={() => setOpen(true)}>≡</button>
+					}
+				</div>
 			</div>
 		</>
 	)
