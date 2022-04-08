928a1a1bbe965f5ce4cfc760d230298e658c62b0
Nimplex <39964594+Nimplex@users.noreply.github.com>
2022-04-08 23:37:42 +0200

    Fixed DiscordAPIError unkown role

--- backend/src/bot/plugins/server/commands/verification.ts
+++ backend/src/bot/plugins/server/commands/verification.ts
@@ -4,15 +4,16 @@ import { CommandArgs } from "../../../../types";
 
 export const execute = async function({ client, message, args }: CommandArgs) {
 	const role = message.mentions.roles.first();
+	
 	if (!role) return message.channel.send({
 		embeds: [ErrorEmbed(message, "Nie spingowano roli, którą mam nadać po weryfikacji!")]
 	});
 
-	const msg = await message.channel.send({ content: "Zweryfikuj się naciskając emoji pod wiadomością! Weryfikując się akceptujesz <#843484880116514830>" });
-	await msg.react("❤️");
-
 	client.Core.database.settings.set("verificationChannel", message.channel.id);
 	client.Core.database.settings.set("verificationRole", role.id);
+
+	const msg = await message.channel.send({ content: "Zweryfikuj się naciskając emoji pod wiadomością! Weryfikując się akceptujesz <#843484880116514830>" });
+	await msg.react("❤️");
 
 	const verChannel = client.Core.database.settings.get("verificationChannel");
 	const verRole = client.Core.database.settings.get("verificationRole");
