e97249adce84d02f4d2c6fd17b9a6e657d55f664
Nimplex <39964594+Nimplex@users.noreply.github.com>
2023-04-17 22:47:54 +0200

    smoother experience i guess

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -7,7 +7,7 @@ import Loading from './compontents/Loading/Loading'
 import Navbar from './compontents/Navbar/Navbar'
 import Footer from './compontents/Footer/Footer'
 
-const Home = lazy(() => import('./sites/Home/Home'))
+import Home from './sites/Home/Home'
 const Articles = lazy(() => import('./sites/Articles/Articles'))
 const NotFound = lazy(() => import('./sites/NotFound/NotFound'))
 const Article = lazy(() => import('./sites/Articles/Article'))
@@ -31,22 +31,10 @@ const App = () => {
 				<div id="content-container">
 					<Routes>
 						<Route path="/">
-							<Route
-								index
-								element={<Home />}
-								loader={() => <Loading />}
-							/>
+							<Route index element={<Home />} />
 							<Route path="articles">
-								<Route
-									index
-									element={<Articles />}
-									loader={() => <Loading />}
-								/>
-								<Route
-									path=":id"
-									element={<Article />}
-									loader={() => <Loading />}
-								/>
+								<Route index element={<Articles />} />
+								<Route path=":id" element={<Article />} />
 							</Route>
 							{/* <Route path="about" element={<About />} /> */}
 							{/* <Route path="dashboard" element={<Dashboard />} /> */}
@@ -64,7 +52,7 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
 	<React.StrictMode>
 		<BrowserRouter>
 			<RecoilRoot>
-				<Suspense fallback={<Loading />}>
+				<Suspense>
 					<App />
 				</Suspense>
 			</RecoilRoot>
