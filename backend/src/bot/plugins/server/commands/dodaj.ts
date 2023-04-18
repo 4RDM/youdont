7e79ce78f430d5c74200eac820305d6446e62d14
Nimplex <39964594+Nimplex@users.noreply.github.com>
2023-04-18 20:43:43 +0200

    Embeds

--- backend/src/bot/plugins/server/commands/dodaj.ts
+++ backend/src/bot/plugins/server/commands/dodaj.ts
@@ -1,4 +1,4 @@
-import { ErrorEmbed } from "../../../../utils/discordEmbed";
+import { Embed, ErrorEmbed } from "../../../../utils/discordEmbed";
 import { addFile } from "../../../../utils/filesystem";
 import { hexToDec } from "../../../../utils/strings";
 import { CommandArgs } from "../../../../types";
@@ -14,21 +14,40 @@ export const execute = async function ({ message, args, client }: CommandArgs) {
 				),
 			],
 		});
-	const msg = await message.channel.send("Wysyłanie polecenia do konsoli");
+
+	const msg = await message.channel.send({
+		embeds: [
+			Embed({
+				description: "**Wysyłanie**",
+				user: message.author,
+			}),
+		],
+	});
+
 	addFile(
 		`add_principal identifier.steam:${args[0]} group.${args[1]} # ${
 			mention.tag
 		} (${mention.id}) https://steamcommunity.com/profiles/${hexToDec(
 			args[0]
 		)} ${new Date().toLocaleDateString()}`,
-		"F:\\fivem-server\\base\\permisje.cfg"
+		"/home/rdm/server/data/permisje.cfg"
 	)
 		.then(() => {
-			msg.edit("Pomyślnie wysłano");
+			msg.edit({
+				embeds: [
+					Embed({
+						color: "#1F8B4C",
+						description: "**Wysłano!**",
+						user: message.author,
+					}),
+				],
+			});
 			client.Core.rcon.send("reload");
 		})
 		.catch(() => {
-			msg.edit("Wystąpił błąd");
+			msg.edit({
+				embeds: [ErrorEmbed(message, "Nie udało się wysłać polecenia")],
+			});
 		});
 };
 
