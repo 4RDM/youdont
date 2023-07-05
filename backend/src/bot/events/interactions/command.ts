186759e26164179d8d665ef1a37c6b97add3efd1
Nimplex <frekerhugug@gmail.con>
2023-07-05 07:58:57 +0200

    napraw uprawnienia mowili

--- backend/src/bot/events/interactions/command.ts
+++ backend/src/bot/events/interactions/command.ts
@@ -9,18 +9,16 @@ export const handleCommandInteraction = async(client: Client, interaction: Comma
 	const command = client.commandHandler.get(interaction.commandName);
 
 	if (command) {
-		if (command.info.permissions && command.info.role)
+		if (command.info.permissions && command.info.role) {
 			if (!interaction.memberPermissions.has(command.info.permissions) && !(interaction.member.roles as GuildMemberRoleManager).cache.has(command.info.role))
 				return interaction.Reply({ content: "Nie posiadasz wymaganych uprawień do tego polecenia!", ephemeral: true });
-		
-		if (command.info.permissions)
+		} else if (command.info.permissions) {
 			if (!interaction.memberPermissions.has(command.info.permissions))
 				return interaction.Reply({ content: "Nie posiadasz wymaganych uprawień do tego polecenia!", ephemeral: true });
-		
-		if (command.info.role)
+		} else if (command.info.role) {
 			if (!(interaction.member.roles as GuildMemberRoleManager).cache.has(command.info.role))
 				return interaction.Reply({ content: "Nie posiadasz wymaganych uprawień do tego polecenia!", ephemeral: true });
-		
+		}
 		command.execute({ client, interaction });
 	}
 };
