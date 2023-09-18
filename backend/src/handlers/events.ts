638de472d7c795a293d8063cad1279a077b3cd1e
Nimplex <39964594+Nimplex@users.noreply.github.com>
2023-09-18 21:22:15 +0200

    feat: implement interaction Reply

--- backend/src/handlers/events.ts
+++ backend/src/handlers/events.ts
@@ -5,6 +5,8 @@ import { handleInteraction } from "./interactions";
 export class EventHandler {
     constructor(private client: RDMBot) {
         this.client.once("ready", (client) => logger.ready(`${client.user.tag} is ready!`));
-        this.client.on("interactionCreate", (interaction) => handleInteraction(interaction));
+        this.client.on("interactionCreate", (interaction) => {
+            handleInteraction(interaction, this.client);
+        });
     }
 }
--- backend/src/handlers/interactions.ts
+++ backend/src/handlers/interactions.ts
@@ -1,7 +1,48 @@
-import { Interaction } from "discord.js";
+import { Interaction, InteractionReplyOptions } from "discord.js";
+import { RDMBot } from "main";
+import { EmbedBuilder as EB, ErrorEmbedInteraction } from "utils/embedBuilder";
 
-export const handleInteraction = (interaction: Interaction) => {
+interface _ReplyOptions extends InteractionReplyOptions {
+    isError: boolean
+}
+
+declare module "discord.js" {
+    interface BaseInteraction {
+        hasReplied: boolean;
+        Reply: (content: string | EB[], options?: _ReplyOptions) => Promise<Message<boolean> | InteractionResponse<boolean> | undefined>;
+    }
+}
+
+export const handleInteraction = (interaction: Interaction, client: RDMBot) => {
     if (!interaction.inGuild()) return;
 
+    interaction.Reply = async (content: string | EB[], options?: _ReplyOptions) => {
+        if (!interaction.isRepliable()) return undefined;
+
+        let messageObject = {};
+
+        if (typeof content == "string")
+            messageObject = Object.assign(options || {}, { content });
+        else if (content[0] instanceof EB)
+            messageObject = Object.assign(options || {}, { embeds: content });
+
+        if (interaction.hasReplied) {
+            if (options?.isError)
+                return interaction.Reply([ErrorEmbedInteraction(interaction, content as string)]);
+            return interaction.deferReply(messageObject);
+        } else {
+            if (options?.isError)
+                return interaction.Reply([ErrorEmbedInteraction(interaction, content as string)]);
+            interaction.hasReplied = true;
+            return interaction.reply(messageObject);
+        }
+    };
+
+    if (interaction.isCommand()) {
+        const command = client.commands.get(interaction.commandName);
+
+        if (!command)
+            return interaction.Reply("Command not found!", { isError: true });
+    }
 
 };
