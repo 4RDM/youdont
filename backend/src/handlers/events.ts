142bd04f9a00a77ec8005d4a255d87d3f3c35b5b
Nimplex <39964594+Nimplex@users.noreply.github.com>
2023-11-21 20:19:12 +0100

    implement `status`

--- backend/src/handlers/events.ts
+++ backend/src/handlers/events.ts
@@ -1,7 +1,7 @@
 import { RDMBot } from "main";
 import logger from "utils/logger";
 import { handleInteraction } from "./interactions";
-import { getTops } from "utils/serverStatus";
+import { getPlayers, getTops } from "utils/serverStatus";
 import { Channel, TextChannel } from "discord.js";
 import { Embed } from "utils/embedBuilder";
 import { embedColors } from "utils/constants";
@@ -67,6 +67,61 @@ const reloadStats = async (client: RDMBot, statsChannel: TextChannel) => {
     }
 };
 
+const reloadStatus = async (client: RDMBot, statusChannel: TextChannel) => {
+    const status = await getPlayers();
+
+    const statusMessage = await client.database.config.get("statusMessageID");
+
+    if (statusMessage === false)
+        return logger.error("reloadStatus(): Database error while fetching statusMessageID");
+
+    if (statusMessage === null)
+        return logger.error("reloadStatus(): statusMessageID not set");
+
+    if (!status) {
+        const embed = Embed({
+            author: {
+                name: "4RDM",
+                iconURL: "https://4rdm.pl/assets/logo.png"
+            },
+            color: embedColors.red,
+            title: "<a:odrzucone:846767715807920159> | 4RDM jest offline!",
+            footer: "Status aktualizuje się co 5 sekund",
+            timestamp: new Date()
+        });
+
+        return await statusChannel.messages.edit(statusMessage, {
+            embeds: [ embed ],
+            content: "",
+        });
+    }
+
+    const embed = Embed({
+        author: {
+            name: "4RDM",
+            iconURL: "https://4rdm.pl/assets/logo.png"
+        },
+        title: "<a:zaakceptowane:846767788739657738> | 4RDM jest online!",
+        description: `**Graczy online:** ${status.length}/${client.config.maxPlayers}`,
+        color: embedColors.green,
+        footer: "Status aktualizuje się co 5 sekund",
+        timestamp: new Date()
+    });
+
+    const playerEmbed = Embed({
+        title: "<:coin:1176598132683964517> | Lista graczy",
+        description: `${status.sort((a, b) => a.id - b.id).map((player) => `**${player.id}.** \`${player.name.replace(/`/gm, "")}\``).join("\n")}`,
+        color: embedColors.green,
+        footer: "Status aktualizuje się co 5 sekund",
+        timestamp: new Date()
+    });
+
+    return await statusChannel.messages.edit(statusMessage, {
+        embeds: [ embed, playerEmbed ],
+        content: "",
+    });
+};
+
 export class EventHandler {
     constructor(private client: RDMBot) {
         client.once("ready", async (readyClient) => {
@@ -74,6 +129,7 @@ export class EventHandler {
                 logger.ready(`${readyClient.user.tag} is ready!`);
 
                 const statsChannelID = await client.database.config.get("statsChannelID");
+                const statusChannelID = await client.database.config.get("statusChannelID");
 
                 if (statsChannelID === false)
                     return logger.error("EventHandler(): Database error while fetching statsChannelID") as unknown as void;
@@ -81,10 +137,26 @@ export class EventHandler {
                 if (statsChannelID === null)
                     return logger.error("EventHandler(): statsChannelID not set") as unknown as void;
 
+                if (statusChannelID === false)
+                    return logger.error("EventHandler(): Database error while fetching statusChannelID") as unknown as void;
+
+                if (statusChannelID === null)
+                    return logger.error("EventHandler(): statusChannelID not set") as unknown as void;
+
                 const statsChannel = (await readyClient.channels.fetch(statsChannelID, { cache: true, force: true }).catch(() => false)) as Channel | false;
+                const statusChannel = (await readyClient.channels.fetch(statusChannelID, { cache: true, force: true }).catch(() => false)) as Channel | false;
 
                 if (!statsChannel || statsChannel == null)
                     return logger.error("EventHandler(): Stats channel not found!") as unknown as void;
+
+                if (!statusChannel || statusChannel == null)
+                    return logger.error("EventHandler(): Status channel not found!") as unknown as void;
+
+                if (!statusChannel.isTextBased())
+                    return logger.error("EventHandler(): Status channel is not text based!") as unknown as void;
+
+                if (statusChannel.isDMBased())
+                    return logger.error("EventHandler(): Status channel is DM based!") as unknown as void;
 
                 if (!statsChannel.isTextBased())
                     return logger.error("EventHandler(): Stats channel is not text based!") as unknown as void;
@@ -94,6 +166,10 @@ export class EventHandler {
 
                 setInterval(async () => {
                     await reloadStats(client, statsChannel as TextChannel);
+                }, 1000 * 60 * 10);
+
+                setInterval(async () => {
+                    await reloadStatus(client, statusChannel as TextChannel);
                 }, 5000);
             } catch(err) {
                 logger.error(`EventHandler(): Ready handler threw an error: ${err}`);
