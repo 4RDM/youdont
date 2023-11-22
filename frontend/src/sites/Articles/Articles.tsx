c315c023ec25d13984a98fbd95a7a3d22cd7f46e
Nimplex <39964594+Nimplex@users.noreply.github.com>
2023-11-22 20:09:11 +0100

    load page title before content

--- frontend/src/sites/Articles/Articles.tsx
+++ frontend/src/sites/Articles/Articles.tsx
@@ -43,50 +43,51 @@ export default () => {
 			})
 	}, [])
 
-	return isLoading ? (
-		<Loading />
-	) : (
+	return (
 		<div id="articles-container">
 			<div id="articles-head">
 				<h1>Artykuły</h1>
 				{Math.floor(Math.random() * 500) == 420 && <img className="kotel" src={kotel}></img>}
 			</div>
-			<div id="articles-list">
-				{articles.map((article, i) => (
-					<article>
-					<Link to={`/articles/${article.articleURL}`} key={i}>
-						<div
-							className="article-card"
-							style={{
-								backgroundImage: 'none',
-							}}
-						>
-							<h1 className="article-card-header">
-								{article.title}
-							</h1>
-							<p className="article-card-description">
-								{article.articleDescription}
-							</p>
-							<div className="article-card-sub">
-								<div className="article-card-author">
-									<img
-										src={article.discordAvatar}
-										alt="Awatar autora"
-										crossOrigin="anonymous"
-									/>
-									<p className="article-author">
-										{article.discordName}
+			{isLoading ?
+				<Loading /> :
+				<div id="articles-list">
+					{articles.map((article, i) => (
+						<article>
+						<Link to={`/articles/${article.articleURL}`} key={i}>
+							<div
+								className="article-card"
+								style={{
+									backgroundImage: 'none',
+								}}
+							>
+								<h1 className="article-card-header">
+									{article.title}
+								</h1>
+								<p className="article-card-description">
+									{article.articleDescription}
+								</p>
+								<div className="article-card-sub">
+									<div className="article-card-author">
+										<img
+											src={article.discordAvatar}
+											alt="Awatar autora"
+											crossOrigin="anonymous"
+										/>
+										<p className="article-author">
+											{article.discordName}
+										</p>
+									</div>
+									<p className="article-views">
+										<Eye size={15} /> {article.views}
 									</p>
 								</div>
-								<p className="article-views">
-									<Eye size={15} /> {article.views}
-								</p>
 							</div>
-						</div>
-					</Link>
-					</article>
-				))}
-			</div>
+						</Link>
+						</article>
+					))}
+				</div>
+		}
 		</div>
 	)
 }
