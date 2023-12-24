d6ebd7550d4fd1ed0498ba9ccd7ac8be4a53b0de
Nimplex <39964594+Nimplex@users.noreply.github.com>
2023-12-24 23:54:40 +0100

    Update database.ts

--- backend/src/database/database.ts
+++ backend/src/database/database.ts
@@ -1,6 +1,6 @@
 import { EventEmitter } from "events";
 import { RDMBot } from "main";
-import mariadb from "mariadb";
+import mariadb, { PoolConfig } from "mariadb";
 import logger from "utils/logger";
 import { UsersManager } from "./users";
 import { PaymentsManager } from "./payments";
@@ -17,8 +17,8 @@ export interface OkPacketInterface {
 }
 
 export class Database extends EventEmitter {
-    private serverPool;
-    private botPool;
+    public serverPool;
+    public botPool;
     public devMode;
     public users;
     public payments;
@@ -31,9 +31,13 @@ export class Database extends EventEmitter {
     constructor(private client: RDMBot) {
         super();
 
+        const config: PoolConfig = {
+            connectionLimit: 100,
+        };
+
         this.devMode = client.devMode;
-        this.serverPool = mariadb.createPool({ ...client.config.fivemDB });
-        this.botPool = mariadb.createPool({ ...client.config.botDB });
+        this.serverPool = mariadb.createPool({ ...client.config.fivemDB, ...config });
+        this.botPool = mariadb.createPool({ ...client.config.botDB, ...config });
 
         this.users = new UsersManager(this);
         this.payments = new PaymentsManager(this, this.client.config.indrop.key);
@@ -58,10 +62,19 @@ export class Database extends EventEmitter {
         return await this.serverPool.getConnection();
     }
 
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    async queryBot(query: string, values?: any) {
+        return await this.botPool.query(query, values);
+    }
+
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    async queryServer(query: string, values?: any) {
+        return await this.serverPool.query(query, values);
+    }
+
     async testConnection() {
         const conn1 = await this.getBotConnection().then(() => "OK").catch(() => "ERROR");
         const conn2 = await this.getServerConnection().then(() => "OK").catch(() => "ERROR");
-
 
         if ((conn1 + conn2).includes("ERROR")) {
             logger.error(`Bot database: ${conn1}`);
