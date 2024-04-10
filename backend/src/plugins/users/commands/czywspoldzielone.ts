b7afff941f70b449f3baa7b5148fae9afb3ae538
kubamaz <64901069+kubamaz@users.noreply.github.com>
2024-04-10 23:18:06 +0200

    Create czywspoldzielone.ts

---
+++ backend/src/plugins/users/commands/czywspoldzielone.ts
@@ -0,0 +1,55 @@
+import { existsSync, writeFileSync } from "fs";
+import { join } from "path";
+import { PermissionFlagsBits, SlashCommandBuilder } from "discord.js";
+import { CommandArgs, CommandInfoType } from "handlers/commands";
+import { Embed } from "utils/embedBuilder";
+import { Roles, embedColors } from "utils/constants";
+import { readFile } from "fs/promises";
+
+const filePath = join(
+    // __dirname,
+    // "vehicles.json"
+    "/home/rdm/server/data/resources/[4rdm]/4rdm/data/auta/shared.json"
+    // "D:\\server\\server\\resources\\[4rdm]\\4rdm\\data\\auta\\shared.json" //K3iX WINDOWS XD
+);
+
+export default async function ({ client, interaction }: CommandArgs) {
+    if (!interaction.isChatInputCommand()) return;
+    if (!existsSync(filePath))
+        return await interaction.Error("Funkcja niedostępna na tym komputerze!");
+
+    const file = await readFile(filePath, { encoding: "utf-8" });
+    const json = JSON.parse(file);
+    const sharedJson = json;
+    const spawnName = interaction.options.getString("spawn-name", true);
+    
+    for (const hex in sharedJson) {
+        const entries = sharedJson[hex];
+        for (const [respname, _] of entries) {
+            if (respname.toLowerCase() === spawnName.toLowerCase()) {
+                const embed = Embed({
+                    title: ":white_check_mark: | Znaleziono wspóldzielenie!",
+                    color: embedColors.green,
+                    user: interaction.user,
+                })
+                return await interaction.Reply([embed]);
+            }
+        }
+    }
+    const embed = Embed({
+        title: ":x: | Nie znaleziono wspóldzielenia!",
+        color: embedColors.red,
+        user: interaction.user,
+    })
+    return await interaction.Reply([embed]);
+}
+
+export const info: CommandInfoType = {
+    name: "czywspoldzielone",
+    description: "Sprawdź czy ktoś ma współdzieloną daną limitkę",
+    permissions: PermissionFlagsBits.Administrator,
+    role: [ Roles.Owner, Roles.Zarzad, Roles.HeadAdmin, Roles.Developer, Roles.TrialDeveloper ],
+    builder: new SlashCommandBuilder()
+    .addStringOption(option => option.setName("spawn-name").setDescription("Spawn name").setRequired(true))
+    .setName("czywspoldzielone"),
+};
